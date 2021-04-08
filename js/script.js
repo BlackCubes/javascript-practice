f82cfe5cf24b45238f7ecaa7222813667658d0fb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-08 10:49:21 -0700

    Added: a single-line comment, a multi-line comment, console logged my name, and console logged my favorite food

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// This is a single-line comment :)
+
+/*
+  This is a multi-line comment :))
+*/
+
+console.log("My name is Elias Gutierrez...");
+console.log("And my favorite food is Scampi Shrimp. YUM!");
